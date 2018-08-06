86b98e09d6e5aac34865343f2cc2d2c49effd463
glenr4 <glenrutherford4@gmail.com>
2018-08-06 17:23:28 +1000

    Added movie count

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -7,37 +7,45 @@ class Movies extends Component {
   };
 
   render() {
-    console.log(this.state.movies);
+    const { length: movieCount } = this.state.movies;
+
+    if (movieCount === 0) {
+      return <p>There are no movies in the database</p>;
+    }
+
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Genre</th>
-            <th>Stock</th>
-            <th>Rate</th>
-            <th />
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.movies.map(m => (
-            <tr key={m._id}>
-              <td>{m.title}</td>
-              <td>{m.genre.name}</td>
-              <td>{m.numberInStock}</td>
-              <td>{m.dailyRentalRate}</td>
-              <td>
-                <button
-                  onClick={() => this.handleDelete(m)}
-                  className="btn btn-danger btn-sm"
-                >
-                  Delete
-                </button>
-              </td>
+      <React.Fragment>
+        <p>There are {movieCount} movies in the database</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th />
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {this.state.movies.map(m => (
+              <tr key={m._id}>
+                <td>{m.title}</td>
+                <td>{m.genre.name}</td>
+                <td>{m.numberInStock}</td>
+                <td>{m.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(m)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
     );
   }
 
