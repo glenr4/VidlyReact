fa93c7ed7ed4a6b6f8260beade23a90772a6e443
glenr4 <glenrutherford4@gmail.com>
2018-08-06 10:25:28 +1000

    Init

---
+++ src/components/movies.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies()
+  };
+
+  render() {
+    console.log(this.state.movies);
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Genre</th>
+            <th>Stock</th>
+            <th>Rate</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.movies.map(m => (
+            <tr key={m._id}>
+              <td>{m.title}</td>
+              <td>{m.genre.name}</td>
+              <td>{m.numberInStock}</td>
+              <td>{m.dailyRentalRate}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default Movies;
